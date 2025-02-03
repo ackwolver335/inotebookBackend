67687e1d036b5bba35aa66c82412afc9cca1c9c9
ackwolver335 <abhaych335@gmail.com>
2025-02-03 11:03:49 +0530

    Connection Data Established

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.send('Hello Ack Wolver!')
 })
 
+app.get('/api/login', (req, res) => {             // for login of the user
+  res.send('Hello Ack Wolver!')
+})
+
+app.get('/api/signup', (req, res) => {            // for signup page here
+  res.send('Hello User Sign Up here!')
+})
+
+// available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
