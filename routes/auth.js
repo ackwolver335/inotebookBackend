f997c021404d9f38337772f11b8750cfaa1dca97
ackwolver335 <abhaych335@gmail.com>
2025-02-04 09:29:37 +0530

    Working with Data Validations here

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { query, validationResult } = require('express-validator');
+const { ValidationHalt } = require('express-validator/lib/base');
+const app = express();
 
 
 // Creating a User using: POST "/api/auth". Doesn't requires Auth
-router.post('/',(req,res) => {
-    console.log(req.body);
+// router.get('/',(req,res) => {
+//     console.log(req.body);                          // getting the incoming data
+//     const user = User(req.body);                    // Creating a new user here
+//     user.save();                                    // saving the user here
+//     res.send(req.body);
+// })
+
+router.post('/',[
+
+    // Applying different validation checks here
+    query('name').isLength({ min : 4}),
+    query('username').isLength({ min : 8 }),
+    query('email').isEmail(),
+    query('password').isLength({ min : 8})
+
+],(req,res) => {
+    const result = validationResult(req);
     const user = User(req.body);                    // Creating a new user here
     user.save();                                    // saving the user here
-    res.send(req.body);
+    
+    // applying the condition here
+    if(!(result.isEmpty())){
+        return res.send(`Response ,${req.query.name}!`);
+    }
+
+    res.send({ errors : result.array()});
 })
 
 module.exports = router;
