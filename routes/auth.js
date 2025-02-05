f44539b729de53fba296d8cb48310a67311d80de
ackwolver335 <abhaych335@gmail.com>
2025-02-05 12:27:18 +0530

    Firs Method for Adjusting the Errors

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const { query, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
-
-// Creating a User using: POST "/api/auth". Doesn't requires Auth
-// router.get('/',(req,res) => {
-//     console.log(req.body);                          // getting the incoming data
-//     const user = User(req.body);                    // Creating a new user here
-//     user.save();                                    // saving the user here
-//     res.send(req.body);
-// })
 
 router.post('/',[
 
-    // Applying different validation checks here
-    query('name').isLength({ min : 4}),
-    query('username').isLength({ min : 8 }),
-    query('email').isEmail(),
-    query('password').isLength({ min : 8})
+    // Applying different validation checks here,
+    body('username').isLength({ min : 8 }).withMessage('Username must contains atleast 8 characters !'),
+    body('email').isEmail().withMessage('Invalid Email Format'),
+    body('password').isStrongPassword().isLength({ min : 8}).withMessage('Password must contain Lowercase, Uppercase, Numerical Values & Symbols in it !')
 
-],(req,res) => {
+],async (req,res) => {
+       
+    // Checking if there is any kind of errors here regarding the incoming data
     const result = validationResult(req);
-    const user = User(req.body);                    // Creating a new user here
-    user.save();                                    // saving the user here
     
     // applying the condition here
-    if(result.isEmpty()){
-        return res.send(`Response ,${req.query}!`);
+    if(!result.isEmpty()){                           // Checking if there is any kind of errors
+        return res.status(400).json({result : result.array()});
     }
 
-    res.send({ errors : result.array()});
+    // Applying a try catch here in order to maintain the error handling in case of creating a user here
+    try{
+        const user = User(req.body);
+        await user.save();                      // Saving the user's details after a proper confirmation check
+
+        // Proper message towards the developer
+        res.status(200).json({message : "User Created Successfully !",user});
+    } 
+    
+    // Error Checks regarding different kinds of issues before any user gets created
+    catch (error){
+        console.error(error);
+        res.status(500).json({message : "Internal Server Error !"});
+    }
 })
 
 module.exports = router;
